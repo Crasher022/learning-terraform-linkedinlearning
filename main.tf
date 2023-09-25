fbc37e1c8ce6fbf6060488fe6eecc9402e75de8b
Crasher022 <145906917+Crasher022@users.noreply.github.com>
2023-09-25 22:07:17 +0200

    create dev vpc 1.2

--- main.tf
+++ main.tf
@@ -17,14 +17,14 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
+  name = "dev "
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-
+  
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -45,13 +45,12 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
+  
   name    = "blog"
-
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
